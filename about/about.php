3b1570dd48f2dcf19f3f7f3d745edf01ff542e47
Marielle Limbo <mawyeeel.school@gmail.com>
2022-05-31 21:03:59 +0800

    added files

---
+++ about/about.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+<link rel="stylesheet" type="text/css" href="about_style.css">
+</head>
+<body>
+    <div class= "content">
+        <div>
+            <h1 class='title'>All About Us</h1>
+            </br>
+                </br>
+                </br>
+                </br>
+            <img class='profile1' src='img/Marianne.jpg'>
+        </div>
+        <div class= "maincontent">
+        <?php
+
+                echo "<div class='personalinfo1'>
+                <strong>Name:</strong> Marianne Ebarola Motol</br>
+                <strong>Address:</strong> Pangi Gasan Marinduque</br>
+                <strong>Age:</strong> 20 years old </br>
+                <strong>Gender:</strong> Female</br>
+                <strong>Religion:</strong> Catholic </br>
+                </br>
+                <h1 class='education1'>Educational Background</h1>
+                <strong>Primary:</strong><a>Pangi Elementary School</a></br>
+                <strong>Secondary (Junior):</strong><a>Bangbang National High School</a></br>
+                <strong>Secondary (Senior):</strong><a>Marinduque Midwest College</a>";
+        ?>
+        </div>
+
+        <div class="interest1">
+            <?php
+            echo "<div>
+            </br>
+            <h1>My Interest</h1>
+            <div>
+            I love dancing</br>
+            I like to Create Stories.</br>
+            I also watch Hollywood Movies during my siesta time.</br>
+            I am fond of Reading Books.</br>";           
+                        
+            ?>
+        </div>
+
+        <div class="movies">
+            <?php
+            echo "<div>
+                </br>
+                </br>
+                <h1 class='movie'>Movies I Love</h1></br>
+                </br>
+                <h5 class='textwave'>The 5th Wave</h5>
+                <img class='wave' src='img/theWave.jpg'></br>
+                </br>
+                <h5 class='textsense'>The Sixth Sense<h5>
+                <img class='sense' src='img/sixthSense.png'></br>
+                </br>
+                <h5 class='textchild'>Wild Child</h5>
+                <img class='child' src='img/wildChild.jpg'></br>
+                </br>
+                <h5 class='textafter'>After</h5>
+                <img class='after' src='img/after.jpg'>"
+            ?>
+        </br>
+        </div>
+        </div>
+
+        <div>
+              </br>
+                </br>
+            </br>
+                </br>
+            <img class='profile2' src='img/Marielle.jpg'>
+        </div>
+        <div class= "maincontent">
+        <?php
+
+                echo "<div class='personalinfo2'>
+                <strong>Name:</strong> Marielle Angel Charisse M. Limbo</br>
+                <strong>Address:</strong> Bantad Boac Marinduque</br>
+                <strong>Age:</strong> 20 years old </br>
+                <strong>Gender:</strong> Female</br>
+                <strong>Religion:</strong> Catholic </br>
+                </br>
+                <h1 class='education2'>Educational Background</h1>
+                <strong>Primary:</strong><a> Caingagan Elementary School</a></br>
+                <strong>Secondary (Junior):</strong><a> Buenavista National High School</a></br>
+                <strong>Secondary (Senior):</strong><a> Marinduque State College</a>";
+        ?>
+        </div>
+
+        <div class="interest2">
+            <?php
+            echo "<div>
+            </br>
+            <h1>My Interest</h1>
+            <div>
+            I love reading books</br>
+            I also watch movies, series and documentaries.</br>
+            I am fond of Baking.</br>";           
+                        
+            ?>
+        </div>
+
+        <div class="movies">
+            <?php
+            echo "<div>
+                </br>
+                </br>
+                <h1 class='movie'>Movies I Love</h1></br>
+                </br>
+                <h5 class='textparadox'>The Cloverfield Paradox</h5>
+                <img class='paradox' src='img/paradox.jpg'></br>
+                </br>
+                <h5 class='textanna'>Anna<h5>
+                <img class='anna' src='img/anna.jpg'></br>
+                </br>
+                <h5 class='textamanda'>My Amanda</h5>
+                <img class='amanda' src='img/myAmanda.jpg'>"
+            ?>
+
+        </div>
+        </div>
+
+            <div></br>
+                </br>
+                </br>
+                </br>
+            <img class='profile3' src='img/Kyla.jpg'>
+        </div>
+        <div class= "maincontent">
+        <?php
+
+                echo "<div class='personalinfo3'>
+                <strong>Name:</strong> Kyla  Marisse R. Marciano</br>
+                <strong>Address:</strong> Poctoy Torrijos Marinduque</br>
+                <strong>Age:</strong> 20 years old </br>
+                <strong>Gender:</strong> Female</br>
+                <strong>Religion:</strong> Catholic </br>
+                </br>
+                <h1 class='education3'>Educational Background</h1>
+                <strong>Primary:</strong><a> Poctoy Elementary School</a></br>
+                <strong>Secondary (Junior):</strong><a> Poctoy National High School</a></br>
+                <strong>Secondary (Senior):</strong><a> Poctoy National High School</a>";
+        ?>
+        </div>
+
+        <div class="interest3">
+            <?php
+            echo "<div>
+            </br>
+            <h1>My Interest</h1>
+            <div>
+            I love watching Kdrama</br>
+            I like to Organize things.</br>
+            I am fond of Reading Books.</br>";           
+                        
+            ?>
+        </div>
+
+        <div class="books">
+            <?php
+            echo "<div>
+                </br>
+                </br>
+                <h1 class='movie'>Books I Love</h1></br>
+                </br>
+                <h5 class='textends'>It Ends With Us</h5>
+                <img class='ends' src='img/ends.jpg'></br>
+                </br>
+                <h5 class='textlive'>I Decided to Live as Me</h5>
+                <img class='live' src='img/live.jpg'></br>
+                </br>
+                <h5 class='texthabits'>Atomic Habits</h5>
+                <img class='habits' src='img/atomicHabits.jpg'>"
+            ?>
+
+        </div>
+        </div>
+
+    </div>
+    
+</body>
+</html>
